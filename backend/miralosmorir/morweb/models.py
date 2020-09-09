6bf71ba8070fa68870fd6be47870fea636a75011
U-C-2963\Cassagnes Andres <Cassagnes Andres@C-2963.invap.com.ar>
2020-09-09 12:42:47 -0300

    unique fields

--- backend/miralosmorir/morweb/models.py
+++ backend/miralosmorir/morweb/models.py
@@ -9,14 +9,14 @@ class TagClass(models.Model):
 class Movie(models.Model):
     name = models.CharField(max_length=70, blank=True, default='')
     year = models.CharField(max_length=70, blank=True, default='')
-    link = models.CharField(max_length=70, blank=True, default='')
+    link = models.CharField(max_length=70, unique=True, blank=False, default='')
     words = models.CharField(max_length=70, blank=True, default='')
     mtags = models.ManyToManyField(TagClass, blank=True, related_name='movie_tags')
     def __str__(self):
         return self.name
 
 class MovieList(models.Model):
-    name = models.CharField(max_length=70, blank=True, default='')
+    name = models.CharField(max_length=70, unique=True, blank=False, default='')
     description = models.CharField(max_length=70, blank=True, default='')
     img = models.CharField(max_length=70, blank=True, default='')
     by = models.CharField(max_length=70, blank=True, default='')
