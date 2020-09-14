9fc840f157bc8e69d96f464dfd0019a997120e4f
U-C-2963\Cassagnes Andres <Cassagnes Andres@C-2963.invap.com.ar>
2020-09-14 16:09:19 -0300

    search movie

--- backend/miralosmorir/morweb/admin.py
+++ backend/miralosmorir/morweb/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Movie, MovieList, TagClass
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'words')
+    search_fields = ('name', 'words')
+
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(MovieList)
 admin.site.register(TagClass)
+admin.site.register(MovieAdmin)
